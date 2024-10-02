064832980b27d86aaff1f26af1d85cee73dddc59
MariuzM <info@marius.dev>
2024-10-03 01:43:10 +0700

    up

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -5,4 +5,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [tailwindcss(), react()],
+	build: { minify: 'terser', terserOptions: { format: { comments: false } } },
 })
