7e9caca3572e85d9569deb1f889484887f97cb48
MariuzM <info@marius.dev>
2024-10-03 00:59:41 +0700

    add tv and err route

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -3,12 +3,14 @@ import './styles/main.style.css'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import { ErrorPage } from './app/_error'
 import { Home } from './app/home'
 
 const routes = createBrowserRouter([
 	{
 		path: '/',
 		element: <Home />,
+		errorElement: <ErrorPage />,
 	},
 ])
 
