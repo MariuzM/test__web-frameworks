a329384d7ef7d996663e5ed6ac0fa6845a6a2e4e
MariuzM <info@marius.dev>
2024-10-04 10:42:41 +0700

    react-rr7 - add about route and extra config

--- react-rr7/vite.config.ts
+++ react-rr7/vite.config.ts
@@ -1,8 +1,18 @@
-import tailwindcss from "@tailwindcss/vite";
-import { reactRouter } from "@react-router/dev/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { reactRouter } from '@react-router/dev/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+	plugins: [
+		tailwindcss(),
+		reactRouter({
+			appDirectory: 'app',
+			// async prerender() {
+			// 	return ['/', '/about', '/contact']
+			// },
+		}),
+		tsconfigPaths(),
+	],
+})
