f98abd18ab601ffc5205fa728986df6729aea443
MariuzM <info@marius.dev>
2024-10-06 17:04:32 +0700

    test tw css variables

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-	return <div>Home Page</div>
+	return (
+		<div>
+			<div className="bg-[--primary]">Home Page</div>
+			<div className="bg-[var(--primary)]">Home Page</div>
+		</div>
+	)
 }
