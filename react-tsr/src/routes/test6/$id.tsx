b47457f7e648807486efd44b04ef3a646bced6ff
MariuzM <info@marius.dev>
2024-10-13 15:27:01 +0700

    add loader to param with sleep

--- react-tsr/src/routes/test6/$id.tsx
+++ react-tsr/src/routes/test6/$id.tsx
@@ -1,11 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/test6/$id')({
+	loader: async ({ params }) => {
+		await new Promise((r) => setTimeout(r, 1000))
+		return { id: params.id }
+	},
 	component: Test6,
 })
 
 function Test6() {
-	const { id } = Route.useParams()
+	const { id } = Route.useLoaderData()
 	return (
 		<div>
 			<h1>Test6</h1>
